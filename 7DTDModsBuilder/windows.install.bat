64efd81f19d02d1999ec64df6cb345d655766fcb
DJ BUGI <francescogirellilime@gmail.com>
2023-01-16 00:39:12 +0100

    Added the windows.install.bat file to add the 7dtd-build.bat to the PATH environment variables to be called from cmd

---
+++ 7DTDModsBuilder/windows.install.bat
@@ -0,0 +1,28 @@
+﻿@echo off
+setlocal
+
+rem Get the current directory of the install.bat file
+set scriptDir=%~dp0
+
+rem Set the 7dtd-build.bat file path
+set appDir=%scriptDir%7dtd-build.bat
+
+rem Check if the current directory is already in the PATH environment variable
+set pathExist=false
+for %%i in ("%PATH:;=" "%") do (
+    if "%%~i"=="%appDir%" (
+        set pathExist=true
+        goto pathExistCheck
+    )
+)
+
+:pathExistCheck
+rem Add the current directory to the PATH environment variable if it doesn't exist
+if "%pathExist%"=="false" (
+    setx PATH "%PATH%;%appDir%"
+)
+
+rem Verify that the PATH environment variable was updated
+echo %PATH%
+
+endlocal
